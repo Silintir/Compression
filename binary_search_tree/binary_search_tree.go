46a79e601bbb05acfaba3b382415fb26c1d2788b
Warlock <b.jurczok@gmail.com>
2020-04-01 14:59:40 +0200

    tree working, part of huffman working

--- binary_search_tree/binary_search_tree.go
+++ binary_search_tree/binary_search_tree.go
@@ -7,36 +7,16 @@ import (
 // Node : Basic node to build BST
 type Node struct {
 	Value  float64
-	Symbol byte
+	Symbol int16
 	Left   *Node
 	Right  *Node
 	Parent *Node
 }
 
-// Nodes : type for array of Nodes, to wrap the type for some array specific functions
-type Nodes []*Node
-
-// FindMin - finds the node with the smallest peobablity
-func (n Nodes) FindMin() *Node {
-	min := n[0]
-	for _, node := range n {
-		if node.Value < min.Value {
-			min = node
-		}
-	}
-	return min
-}
-
-// Remove element from slice elegant way
-func (n Nodes) Remove(s []int, i int) []int {
-	s[i] = s[len(s)-1]
-	return s[:len(s)-1]
-}
-
 var nullNode Node = Node{Value: -1, Symbol: 0, Left: nil, Right: nil, Parent: nil}
 
 // MakeNode : wrapper to make creating nodes more convinient
-func MakeNode(v float64, symbol byte) *Node {
+func MakeNode(v float64, symbol int16) *Node {
 	return &Node{Value: v, Symbol: symbol, Left: &nullNode, Right: &nullNode, Parent: &nullNode}
 }
 
@@ -46,7 +26,7 @@ func (root *Node) Insert(node *Node) {
 	var parent *Node = nil
 	for ok := true; ok; ok = (*curr != &nullNode) {
 		parent = *curr
-		if node.Value < parent.Value {
+		if node.Symbol < parent.Symbol {
 			curr = &parent.Left
 		} else {
 			curr = &parent.Right
@@ -67,4 +47,39 @@ func showInOrder(root *Node) {
 		fmt.Println(root.Value)
 		showInOrder(root.Right)
 	}
+}
+
+// Nodes : type for array of Nodes, to wrap the type for some array specific functions
+type Nodes []*Node
+
+// FindIndexMin - finds the index of node with the smallest peobablity
+func (ns Nodes) FindIndexMin() int {
+	min := 0
+	for i, node := range ns {
+		if node.Value < ns[min].Value {
+			min = i
+		}
+	}
+	return min
+}
+
+// PopMin - pop smallest element from slice elegant way
+func (ns Nodes) PopMin() (Nodes, *Node) {
+	minIndex := ns.FindIndexMin()
+	min := ns[minIndex]
+	ns[minIndex] = ns[len(ns)-1]
+	return ns[:len(ns)-1], min
+}
+
+// Pop2Min - pop 2 smallest elements from slice elegant way
+func (ns Nodes) Pop2Min() (Nodes, *Node, *Node) {
+	minIndex := ns.FindIndexMin()
+	min := ns[minIndex]
+	ns[minIndex] = ns[len(ns)-1]
+
+	minIndex2 := ns.FindIndexMin()
+	min2 := ns[minIndex2]
+	ns[minIndex2] = ns[len(ns)-2]
+
+	return ns[:len(ns)-2], min, min2
 }
--- list2/ex1.go
+++ list2/ex1.go
@@ -21,28 +21,37 @@ func main() {
 	file, err := os.Open(os.Args[1])
 	check(err)
 
-	repetitions := make(map[byte]int)
+	repetitions := make(map[byte]float64)
 	curr := make([]byte, 1)
-	counter := 0
+	counter := 0.0
 
 	for {
 		_, err = file.Read(curr)
-		check(err)
-		if err == io.EOF {
+		if err != nil && err == io.EOF {
 			break
 		}
 		repetitions[curr[0]]++
 		counter++
 	}
 
+	classicHuffman(repetitions, counter).Print()
+
 }
 
-func classicHuffman(reps map[byte]int, counter int) {
-	probability := make(bst.Nodes, 0, len(reps))
+func classicHuffman(reps map[byte]float64, counter float64) *bst.Node {
+	ns := make(bst.Nodes, 0, len(reps))
 	for k, v := range reps {
-		probability = append(probability, bst.MakeNode(float64(v/counter), k))
+		ns = append(ns, bst.MakeNode(v/counter, int16(k)))
+	}
+	var newRoot, n1, n2 *bst.Node
+	for fin := false; !fin; fin = (len(ns) == 1) {
+		ns, n1, n2 = ns.Pop2Min()
+		newRoot = bst.MakeNode(n1.Value+n2.Value, -1)
+		newRoot.Insert(n1)
+		newRoot.Insert(n2)
+		ns = append(ns, newRoot)
+
 	}
 
-	//n1, n2 := probability[0], probability[1]
-
+	return ns[0]
 }
