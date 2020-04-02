15be26c40475c9980199ba44d2a642a1642a8826
Warlock <b.jurczok@gmail.com>
2020-04-03 00:37:29 +0200

    some refractor

--- bits/bits.go
+++ bits/bits.go
@@ -1,24 +1,7 @@
 package bits
 
-import (
-	"fmt"
-)
-
 // Bits2bytes converts increments of 8 bits (in string format) to bytes
 func Bits2bytes(bits string) []byte {
-
-	bits2byte := func(b string) byte {
-		multiplier, result := 1, 0
-
-		for i := 7; i >= 0; i-- {
-			if b[i] == '1' {
-				result += multiplier
-			}
-			multiplier *= 2
-		}
-		fmt.Println(b)
-		return byte(result)
-	}
 
 	result := make([]byte, 0, (len(bits) / 8))
 	for i := 0; i < (len(bits) / 8); i++ {
@@ -26,4 +9,16 @@ func Bits2bytes(bits string) []byte {
 	}
 
 	return result
+}
+
+func bits2byte(b string) byte {
+	multiplier, result := 1, 0
+
+	for i := 7; i >= 0; i-- {
+		if b[i] == '1' {
+			result += multiplier
+		}
+		multiplier *= 2
+	}
+	return byte(result)
 }
